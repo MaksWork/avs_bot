d383d448effd0f2b9ff8297efc76dff64ff4ea80
MaksWork <sauageog@gmail.com>
2024-07-03 11:55:06 +0300

    cleaning

--- index.js
+++ index.js
@@ -3,15 +3,13 @@ const TelegramApi = require('node-telegram-bot-api');
 const {app} = require('./server/index.js')
 const { startText, options, switchRegion } = require('./helper');
 
-app.listen(process.env.APP_PORT, () =>{ console.log('server starts on port : ' + process.env.API_PORT)})
+app.listen(process.env.APP_PORT, () =>{ console.log('server starts on port : ' + process.env.APP_PORT)})
 
 const bot_token = process.env.BOT_TOKEN;
 
 const bot = new TelegramApi(bot_token, {polling: true});
 
-
-
-bot.onText('/start', async (msg) =>{//TODO сделать что бы отдельно для юзеров выводилось одно а для меня и шабанова другое
+bot.onText('/start', async (msg) =>{
     const chatId = msg.chat.id
 
     register(chatId);
@@ -20,6 +18,5 @@ bot.onText('/start', async (msg) =>{//TODO сделать что бы отдел
 const register = async (chatId) =>{
     await bot.sendMessage(chatId, `${startText.eng}${startText.rus}${startText.lv}`, options.start_option)
 }
-
 
 module.exports = {bot}
